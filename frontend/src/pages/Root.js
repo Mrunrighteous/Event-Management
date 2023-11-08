1809b6972058e5fc7afe0e685743c791b5a9fac9
Prem kumar S <64196313+Mrunrighteous@users.noreply.github.com>
2023-11-08 22:41:54 +0530

    Added every file

---
+++ frontend/src/pages/Root.js
@@ -0,0 +1,30 @@
+import { useEffect } from "react";
+import { Outlet, useSubmit, useLoaderData } from "react-router-dom";
+
+import MainNavigation from "../components/MainNavigation";
+
+function RootLayout() {
+  // const navigation = useNavigation();
+  const token = useLoaderData();
+  const submit = useSubmit();
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+    setTimeout(() => {
+      submit(null, { action: "/logout", method: "post" });
+    }, 1 * 60 * 60 * 1000);
+  }, [token, submit]);
+
+  return (
+    <>
+      <MainNavigation />
+      <main>
+        {/* {navigation.state === 'loading' && <p>Loading...</p>} */}
+        <Outlet />
+      </main>
+    </>
+  );
+}
+
+export default RootLayout;
